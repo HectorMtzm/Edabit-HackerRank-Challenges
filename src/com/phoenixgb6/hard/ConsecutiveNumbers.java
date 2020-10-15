2b42656ab3609486f80f5b87fc6f5eb4d33aba6d
Hector <hector.mtzm6@gmail.com>
2020-10-15 16:24:56 -0500

    Initial Commit

---
+++ src/com/phoenixgb6/hard/ConsecutiveNumbers.java
@@ -0,0 +1,16 @@
+package com.phoenixgb6.hard;
+
+/*
+function that determines whether elements in an array can be re-arranged to
+form a consecutive list of numbers where each number appears exactly once.
+*/
+
+import org.junit.jupiter.api.Test;
+
+public class ConsecutiveNumbers {
+
+    @Test
+    void test(){
+
+    }
+}
