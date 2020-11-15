555a3121ecc961c6803d259140020536737a4d52
Hector <hector.mtzm6@gmail.com>
2020-11-15 00:54:35 -0600

    added two more

---
+++ src/com/phoenixgb6/expert/CaseAndIndexInverter.java
@@ -0,0 +1,20 @@
+package com.phoenixgb6.expert;
+
+/*
+https://edabit.com/challenge/RdwGosFoQQT6Z82sm
+recursive function that takes a string input and returns the string in a reversed case and order.
+No empty strings and will neither contain special characters nor punctuation.
+HM
+ */
+
+public class CaseAndIndexInverter {
+
+    public static void main(String[] args) {
+        System.out.println(invert("This string is CASE and INDEX reversed"));
+    }
+
+    public static String invert(String str) {
+        if (str.isEmpty()) return str;
+        return invert(str.substring(1)) + ((str.charAt(0) == 32) ? ' ': (char)(str.charAt(0)^32)); //0x20 XOR operation
+    }
+}
---
+++ src/com/phoenixgb6/expert/MinimumStepsToAPalindrome.java
@@ -0,0 +1,29 @@
+package com.phoenixgb6.expert;
+
+/*
+https://edabit.com/challenge/SYTk43tp4y6oA5zGn
+Given an incomplete palindrome as a string, return the minimum letters needed to be
+added on to the end to make the string a palindrome.
+Trivially, words which are already palindromes should return 0.
+HM
+ */
+
+public class MinimumStepsToAPalindrome {
+
+    public static void main(String[] args) {
+        System.out.println(minPalindromeSteps("rorr"));
+        System.out.println(minPalindromeSteps("pole"));
+        System.out.println(minPalindromeSteps("madam"));
+        System.out.println(minPalindromeSteps("mirror"));
+    }
+
+    public static int minPalindromeSteps(String str) {
+        if (str.equals(invert(str))) return 0;
+        return (1 + minPalindromeSteps(str.substring(1)));
+    }
+
+    public static String invert(String str) {
+        if (str.isEmpty()) return str;
+        return invert(str.substring(1)) + str.charAt(0);
+    }
+}
